494adf904a9bebe0dc74f0347462c6359ac180bb
TulebaevTemirlan <47266010+TulebaevTemirlan@users.noreply.github.com>
2020-03-14 13:03:12 +0600

    First App with Angular

---
+++ week4/src/app/products.ts
@@ -0,0 +1,97 @@
+export const products = [
+    {
+      name: 'BOBO Bird',
+      price: '24$',
+      description: 'новинка, мужские деревянные часы с отображением дня недели и даты, кварцевые часы из двухцветного дерева, прямая поставка',
+      rating: '4.9',
+      location: 'https://aliexpress.ru/item/32822148819.html?spm=a2g0o.tm75978.4501445670.3.29437294RHZ6PU&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=1a17dd87-0209-4abc-a95e-897e594aaf6b',
+      image: '/assets/bobo.jpg'
+    },
+    {
+      name: 'Геймпад беспроводной Bluetooth джойстик для PS3',
+      price: '11$',
+      description: 'контроллер Беспроводная консоль для sony Playstation 3 игровой коврик переключатель игры аксессуары',
+      rating: '5.0',
+      location: 'https://aliexpress.ru/item/4000450760281.html?spm=a2g0o.productlist.0.0.566a1f059kbIrn&s=p&algo_pvid=bc2db297-dda8-4af7-a5a1-4eb359b56c00&algo_expid=bc2db297-dda8-4af7-a5a1-4eb359b56c00-2&btsid=0ab6f82315814933531833765e7ea1&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/ps3.jpg'
+    },
+    {
+      name: 'Мужские Винтажные алюминиевые поляризованные солнцезащитные очки',
+      price: '4.98$',
+      description: 'солнцезащитные очки с бабочкой и сердцем, классические брендовые солнцезащитные очки с покрытием, очки для вождения для мужчин/женщин',
+      rating: '5.0',
+      location: 'https://aliexpress.ru/item/4000602585568.html?spm=a2g0o.productlist.0.0.68967fdbH4IDRz',
+      image: '/assets/glasses.jpg'
+    },
+    {
+      name: 'FLOVEME power Bank',
+      price: '11.99',
+      description: '10000 мА/ч, портативное зарядное устройство для samsung Xiaomi Mi, мобильный внешний аккумулятор, power bank, 10000 мА/ч, повербанк, телефон',
+      rating: '4.8',
+      location: 'https://aliexpress.ru/item/33044850111.html?spm=a2g0o.productlist.0.0.6cda782bVeTItq&s=p&algo_pvid=95f5dced-ca6d-46c9-b08b-5e3a00d48364&algo_expid=95f5dced-ca6d-46c9-b08b-5e3a00d48364-0&btsid=0ab50f4915814950736822208e843e&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/powerBank.jpg'
+
+    },
+    {
+      name: 'Ugreen Bluetooth',
+      price: '23.32$',
+      description: 'наушники 5,0 TWS настоящие беспроводные наушники стерео гарнитура для телефона игровая Спортивная гарнитура',
+      rating: '4.9',
+      location: 'https://aliexpress.ru/item/4000188162083.html?spm=a2g0o.productlist.0.0.6e033beeU9cnp6&s=p&algo_pvid=a52b084d-e89f-4efb-97e5-b5690ca59301&algo_expid=a52b084d-e89f-4efb-97e5-b5690ca59301-7&btsid=0ab6f83a15814957857941859e7a4b&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/ugreen.jpg'
+
+    },
+    {
+      name: 'Xiaomi Pocophone F1',
+      price: '305.00$',
+      description: '128 Гб ROM 6 Гб RAM (абсолютно новая/герметичная) poco f1, poco 128, pocof1',
+      rating: '4.9',
+      location: 'https://aliexpress.ru/item/4000348637685.html?spm=a2g0o.productlist.0.0.5c904641BOT5l6&s=p&algo_pvid=4b1d1a0a-6203-4661-a98a-56b567471be1&algo_expid=4b1d1a0a-6203-4661-a98a-56b567471be1-0&btsid=0ab6d59515814959684473977e4b4f&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/phone.jpg'
+
+    },
+    {
+      name: 'NIKE ROSHE',
+      price: '32.76$',
+      description: 'Оригинальные мужские легкие спортивные кроссовки для бега NIKE ROSHE RUN Ourdoors 511881-014',
+      rating: '4.7',
+      location: 'https://aliexpress.ru/item/4000396554480.html?spm=a2g0o.productlist.0.0.6ad82b17AX8wwf&algo_pvid=92cbf042-545e-4614-9d22-5103cf7aa62a&algo_expid=92cbf042-545e-4614-9d22-5103cf7aa62a-6&btsid=0ab6fa8115814960953307008ec625&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/nike.jpg'
+
+    },
+    {
+      name: ' Адидас Нео CS WB',
+      price: '59.50$',
+      description: 'Оригинальное новое поступление, женская спортивная куртка с капюшоном с логотипом Адидас Нео CS WB',
+      rating: '3 заказа было',
+      location: 'https://aliexpress.ru/item/32964749275.html?spm=a2g0o.productlist.0.0.32d42cdex0jPsV&algo_pvid=4df87452-600f-44b7-8d1d-93727799571c&algo_expid=4df87452-600f-44b7-8d1d-93727799571c-13&btsid=0ab6f83a15814962909217983e7a9f&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/adidas.jpg'
+
+    },
+    {
+      name: 'Мотоциклетная Броня',
+      price: '11.90$',
+      description: 'Мотоциклетная Броня куртка полный мотоцикл тела футболка Armor куртка мотокросса сзади плечо протектор шестерни S-XXXL черный',
+      rating: '4.6',
+      location: 'https://aliexpress.ru/item/33052738040.html?spm=a2g0o.productlist.0.0.5c904641BOT5l6&algo_pvid=4b1d1a0a-6203-4661-a98a-56b567471be1&algo_expid=4b1d1a0a-6203-4661-a98a-56b567471be1-8&btsid=0ab6d59515814959684473977e4b4f&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/armor.jpg'
+
+    },
+    {
+      name: 'JEERKOOL Toy Store',
+      price: '10.69$',
+      description: 'Горячая литая модель автомобиля 1:32 Шмель детские металлические игрушки Шевроле Камаро откатные колеса детские подарки на день рождения и Рождество',
+      rating: '4.9',
+      location: 'https://aliexpress.ru/item/4000229538934.html?spm=a2g0o.productlist.0.0.52cb5bb6pZj0HP&s=p&algo_pvid=8ad6677a-b18f-474f-a0f9-06f0f22fcb8d&algo_expid=8ad6677a-b18f-474f-a0f9-06f0f22fcb8d-2&btsid=0ab6f82415814977004057243e05b1&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_',
+      image: '/assets/camaro.jpg'
+
+    }
+
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at http://angular.io/license
+  */
